79a23de4d53c6ca995cab44872bbadfe47a0e380
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-31 18:12:13 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+greeting = "Hello there, and how are you?"
+
+
+def reverse_each_word(string)
+  string.each do |string|
+    string.split(" ")
+    string.each.reverse 
+    reversed = []
+    end
+    reversed << new_string
+  words.reverse {reversed << split_string}
+  reversed.join('')
+
+  end
+reverse_each_word(greeting)
+end
