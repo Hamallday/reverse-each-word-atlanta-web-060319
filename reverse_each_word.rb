c6e6a40c6ffb3ed58f2f98fd021573266cdc3989
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-31 20:28:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,10 +10,10 @@ def reverse_each_word_with_each(string)
 end
 
 def reverse_each_word(string)
-  array = string.split(" ") 
+  array = string.split(" ")
   test_array = []
     array.collect do|string|
-    test_array << string.reverse 
+    test_array << string.reverse
   end
   test_array.join(" ")
 end
